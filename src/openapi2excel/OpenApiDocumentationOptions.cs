e95946d497389b7b94a5e6eb0bf442c2abea4e02
Paweł Szybiak <pszybiak@gmail.com>
2024-03-06 13:35:33 +0100

    Take labels from language dictionary

--- src/openapi2excel/OpenApiDocumentationOptions.cs
+++ src/openapi2excel/OpenApiDocumentationOptions.cs
@@ -2,15 +2,30 @@
 
 public class OpenApiDocumentationOptions
 {
-    public OpenApiDocumentationLanguage Language { get; set; } = [];
+    private OpenApiDocumentationLanguage? _language = null;
+
+    public OpenApiDocumentationLanguage Language
+    {
+        get => _language ?? OpenApiDocumentationLanguage.Default;
+        set => _language = value;
+    }
 }
 
 public class OpenApiDocumentationLanguage : Dictionary<string, string>
 {
     public string Get(string key)
     {
-        return TryGetValue(key, out var result) ? result : string.Empty;
+        return TryGetValue(key, out var result) ? result : "<LANG_ERROR>";
     }
+
+    internal static OpenApiDocumentationLanguage Default
+        => new()
+        {
+            { OpenApiDocumentationLanguageConst.Info, "Info" },
+            { OpenApiDocumentationLanguageConst.Title, "Title" },
+            { OpenApiDocumentationLanguageConst.Version, "Version" },
+            { OpenApiDocumentationLanguageConst.Description, "Description" },
+        };
 }
 
 public static class OpenApiDocumentationLanguageConst
