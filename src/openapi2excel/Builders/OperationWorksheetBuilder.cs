232632a56156c63c76425e8a4d44e02322358ee4
Paweł Szybiak <pszybiak@gmail.com>
2024-03-07 14:58:26 +0100

    Fill operation informations

--- src/openapi2excel/Builders/OperationWorksheetBuilder.cs
+++ src/openapi2excel/Builders/OperationWorksheetBuilder.cs
@@ -6,18 +6,54 @@ namespace openapi2excel.Builders;
 internal class OperationWorksheetBuilder(IXLWorkbook workbook, OpenApiDocumentationOptions options)
     : WorksheetBuilder(options)
 {
+    private int _actualRowIndex = 1;
+    private IXLWorksheet _worksheet = null!;
+
     public IXLWorksheet Build(string path, OpenApiPathItem pathItem, OperationType operationType, OpenApiOperation operation)
     {
-        var worksheet = workbook.Worksheets.Add(operation.OperationId);
+        _actualRowIndex = 1;
+        _worksheet = workbook.Worksheets.Add(operation.OperationId);
+        _worksheet.Column(1).Width = 20;
 
-        var column = 1;
-        var row = 1;
-        worksheet.Cell(row, column).Value = "<<<<<";
-        worksheet.Cell(row++, column).SetHyperlink(new XLHyperlink($"'{InfoWorksheetBuilder.Name}'!A1"));
-        worksheet.Cell(row++, column).Value = path;
-        worksheet.Cell(row++, column).Value = operationType.ToString();
-        worksheet.Cell(row++, column).Value = operation.Summary;
+        AddHomePageLink();
+        AddEmptyRow();
+        AddOperationInfos(path, pathItem, operationType, operation);
+        AddEmptyRow();
 
-        return worksheet;
+        return _worksheet;
+    }
+
+    private void AddOperationInfos(string path, OpenApiPathItem pathItem, OperationType operationType, OpenApiOperation operation)
+    {
+        AddRow(Options.Language.Get(OpenApiDocumentationLanguageConst.OperationType), operationType.ToString().ToUpper());
+
+        AddRow(Options.Language.Get(OpenApiDocumentationLanguageConst.Path), path);
+        AddRow(Options.Language.Get(OpenApiDocumentationLanguageConst.PathDescription), pathItem.Summary);
+        AddRow(Options.Language.Get(OpenApiDocumentationLanguageConst.PathSummary), pathItem.Summary);
+
+        AddRow(Options.Language.Get(OpenApiDocumentationLanguageConst.OperationDescription), operation.Description);
+        AddRow(Options.Language.Get(OpenApiDocumentationLanguageConst.OperationSummary), operation.Summary);
+
+        AddRow(Options.Language.Get(OpenApiDocumentationLanguageConst.Deprecated), Options.Language.Get(operation.Deprecated));
+    }
+
+    private void AddHomePageLink()
+    {
+        _worksheet.Cell(_actualRowIndex, 1).Value = "<<<<<";
+        _worksheet.Cell(_actualRowIndex++, 1).SetHyperlink(new XLHyperlink($"'{InfoWorksheetBuilder.Name}'!A1"));
+    }
+
+    private void AddEmptyRow()
+    {
+        _actualRowIndex++;
+    }
+
+    private void AddRow(string label, string? value, bool addIfNotExists = false)
+    {
+        if (!addIfNotExists && value is null)
+            return;
+
+        _worksheet.Cell(_actualRowIndex, 1).Value = label;
+        _worksheet.Cell(_actualRowIndex++, 2).Value = value;
     }
 }
--- src/openapi2excel/OpenApiDocumentationOptions.cs
+++ src/openapi2excel/OpenApiDocumentationOptions.cs
@@ -2,7 +2,7 @@
 
 public class OpenApiDocumentationOptions
 {
-    private OpenApiDocumentationLanguage? _language = null;
+    private OpenApiDocumentationLanguage? _language;
 
     public OpenApiDocumentationLanguage Language
     {
@@ -14,9 +14,11 @@ public class OpenApiDocumentationOptions
 public class OpenApiDocumentationLanguage : Dictionary<string, string>
 {
     public string Get(string key)
-    {
-        return TryGetValue(key, out var result) ? result : "<LANG_ERROR>";
-    }
+        => TryGetValue(key, out var result) ? result : "<LANG_ERROR>";
+
+    public string Get(bool value)
+        => value ? (TryGetValue(OpenApiDocumentationLanguageConst.Yes, out var yesResult) ? yesResult : "<LANG_ERROR>")
+                 : (TryGetValue(OpenApiDocumentationLanguageConst.No, out var noResult) ? noResult : "<LANG_ERROR>");
 
     internal static OpenApiDocumentationLanguage Default
         => new()
@@ -25,6 +27,18 @@ public class OpenApiDocumentationLanguage : Dictionary<string, string>
             { OpenApiDocumentationLanguageConst.Title, "Title" },
             { OpenApiDocumentationLanguageConst.Version, "Version" },
             { OpenApiDocumentationLanguageConst.Description, "Description" },
+
+            { OpenApiDocumentationLanguageConst.Path, "Path" },
+            { OpenApiDocumentationLanguageConst.PathSummary, "Path summary" },
+            { OpenApiDocumentationLanguageConst.PathDescription, "Path description" },
+
+            { OpenApiDocumentationLanguageConst.OperationType, "Operation type" },
+            { OpenApiDocumentationLanguageConst.OperationSummary, "Operation summary" },
+            { OpenApiDocumentationLanguageConst.OperationDescription, "Operation description" },
+            { OpenApiDocumentationLanguageConst.Deprecated, "Deprecated" },
+
+            { OpenApiDocumentationLanguageConst.Yes, "Yes" },
+            { OpenApiDocumentationLanguageConst.No, "No" }
         };
 }
 
@@ -34,4 +48,16 @@ public static class OpenApiDocumentationLanguageConst
     public const string Title = "Title";
     public const string Version = "Version";
     public const string Description = "Description";
+
+    public const string Path = "Path";
+    public const string PathDescription = "PathDescription";
+    public const string PathSummary = "PathSummary";
+
+    public const string OperationType = "OperationType";
+    public const string OperationDescription = "OperationDescription";
+    public const string OperationSummary = "OperationSummary";
+    public const string Deprecated = "Deprecated";
+
+    public const string Yes = "Yes";
+    public const string No = "No";
 }
