b8f4340a2a3dd7dd49aad34c4dd10834b1b58f79
Paweł Szybiak <pszybiak@gmail.com>
2024-03-16 15:23:52 +0100

    Add CellBuilder

---
+++ src/openapi2excel/Builders/WorksheetPartsBuilders/CellBuilder.cs
@@ -0,0 +1,36 @@
+﻿using ClosedXML.Excel;
+
+namespace OpenApi2Excel.Builders.WorksheetPartsBuilders;
+
+internal class CellBuilder(IXLCell cell, OpenApiDocumentationOptions option)
+{
+    public CellBuilder Next()
+    {
+        cell = cell.CellRight();
+        return this;
+    }
+
+    public CellBuilder Prev()
+    {
+        cell = cell.CellLeft();
+        return this;
+    }
+
+    public CellBuilder WithText(string value)
+        => With(c => c.Value = value);
+
+    public CellBuilder WithBackground(XLColor color)
+        => With(c => c.Style.Fill.BackgroundColor = color);
+
+    public CellBuilder WithHorizontalAlignment(XLAlignmentHorizontalValues alignment)
+        => With(c => c.Style.Alignment.SetHorizontal(alignment));
+
+    public CellBuilder WithBottomBorder()
+        => With(c => c.Style.Border.BottomBorder = XLBorderStyleValues.Medium);
+
+    private CellBuilder With(Action<IXLCell> action)
+    {
+        action(cell);
+        return this;
+    }
+}
