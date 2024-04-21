a37aece2fb20e8093bee1b501d65f20167a84edc
Paweł Szybiak <pszybiak@gmail.com>
2024-04-21 14:10:19 +0200

    InfoWorksheet refactoring

--- src/openapi2excel/Builders/InfoWorksheetsBuilder.cs
+++ src/openapi2excel/Builders/InfoWorksheetsBuilder.cs
@@ -1,5 +1,6 @@
-﻿using ClosedXML.Excel;
+using ClosedXML.Excel;
 using Microsoft.OpenApi.Models;
+using openapi2excel.core.Common;
 
 namespace openapi2excel.core.Builders;
 
@@ -7,83 +8,60 @@ internal class InfoWorksheetBuilder(IXLWorkbook workbook, OpenApiDocumentationOp
    : WorksheetBuilder(options)
 {
    private OpenApiDocument _readResultOpenApiDocument = null!;
-   private IXLWorksheet _infoWorksheet = null!;
+   private IXLWorksheet _worksheet = null!;
    public static string Name => OpenApiDocumentationLanguageConst.Info;
    private int _actualRowIndex = 1;
 
    public IXLWorksheet Build(OpenApiDocument openApiDocument)
    {
       _readResultOpenApiDocument = openApiDocument;
-      _infoWorksheet = workbook.Worksheets.Add(Name);
-      _infoWorksheet.Column(1).Width = 11;
+      _worksheet = workbook.Worksheets.Add(Name);
+      _worksheet.Column(1).Width = 11;
 
       AddVersion();
       AddTitle();
       AddDescription();
-      AddEmptyRow();
 
-      return _infoWorksheet;
+      return _worksheet;
    }
 
    public void AddLink(OperationType operation, string path, IXLWorksheet worksheet)
    {
-      _infoWorksheet.Cell(_actualRowIndex, 1).Value = operation.ToString().ToUpper();
-      _infoWorksheet.Cell(_actualRowIndex, 2).Value = path;
+      var cell = _worksheet.Cell(_actualRowIndex, 1);
+      cell.SetValue(operation.ToString().ToUpper());
+      cell.SetHyperlink(new XLHyperlink($"'{worksheet.Name}'!A1"));
 
-      _infoWorksheet.Cell(_actualRowIndex, 1).SetHyperlink(new XLHyperlink($"'{worksheet.Name}'!A1"));
-      _infoWorksheet.Cell(_actualRowIndex, 2).SetHyperlink(new XLHyperlink($"'{worksheet.Name}'!A1"));
+      cell = cell.CellRight();
+      cell.SetValue(path);
+      cell.SetHyperlink(new XLHyperlink($"'{worksheet.Name}'!A1"));
+
       _actualRowIndex++;
    }
 
-   private void AddVersion()
+   private void AddVersion() => FillInfo(OpenApiDocumentationLanguageConst.Version, _readResultOpenApiDocument.Info.Version);
+
+   private void AddDescription() => FillInfo(OpenApiDocumentationLanguageConst.Description, _readResultOpenApiDocument.Info.Description, true);
+
+   private void AddTitle() => FillInfo(OpenApiDocumentationLanguageConst.Title, _readResultOpenApiDocument.Info.Title);
+
+   private void FillInfo(string languageKey, string value, bool splitMultipleRowText = false)
    {
-      if (string.IsNullOrEmpty(_readResultOpenApiDocument.Info.Version))
+      if (string.IsNullOrEmpty(value))
       {
          return;
       }
 
-      FillInfo(OpenApiDocumentationLanguageConst.Version, _readResultOpenApiDocument.Info.Version);
-   }
-
-   private void AddDescription()
-   {
-      if (string.IsNullOrEmpty(_readResultOpenApiDocument.Info.Description))
+      _worksheet.Cell(_actualRowIndex, 1).Value = Options.Language.Get(languageKey);
+      if (splitMultipleRowText)
       {
-         return;
-      }
-
-      FillInfo(OpenApiDocumentationLanguageConst.Description, _readResultOpenApiDocument.Info.Description, true);
-   }
-
-   private void AddTitle()
-   {
-      if (string.IsNullOrEmpty(_readResultOpenApiDocument.Info.Title))
-      {
-         return;
-      }
-
-      FillInfo(OpenApiDocumentationLanguageConst.Title, _readResultOpenApiDocument.Info.Title);
-   }
-
-   private void AddEmptyRow()
-   {
-      _actualRowIndex++;
-   }
-
-   private void FillInfo(string languageKey, string value, bool multipleRowText = false)
-   {
-      _infoWorksheet.Cell(_actualRowIndex, 1).Value = Options.Language.Get(languageKey);
-      if (multipleRowText)
-      {
-         var splitValues = value.Split('\n', '\r', StringSplitOptions.RemoveEmptyEntries).Select(v => v.Trim()).Where(v => !string.IsNullOrEmpty(v));
-         foreach (var splitValue in splitValues)
-         {
-            _infoWorksheet.Cell(_actualRowIndex++, 2).Value = splitValue;
-         }
+         value.Split('\n', '\r', StringSplitOptions.RemoveEmptyEntries)
+            .Select(v => v.Trim())
+            .Where(v => !string.IsNullOrEmpty(v))
+            .ForEach(splittedValue => _worksheet.Cell(_actualRowIndex++, 2).Value = splittedValue);
       }
       else
       {
-         _infoWorksheet.Cell(_actualRowIndex++, 2).Value = value;
+         _worksheet.Cell(_actualRowIndex++, 2).Value = value;
       }
    }
 }
