94beb08a4435778802064ebff987c7d7b1950f93
Paweł Szybiak <pszybiak@gmail.com>
2024-05-09 07:19:36 +0200

    Set center aligment for Required header property

--- src/openapi2excel/Builders/WorksheetPartsBuilders/ResponseBodyBuilder.cs
+++ src/openapi2excel/Builders/WorksheetPartsBuilders/ResponseBodyBuilder.cs
@@ -57,7 +57,7 @@ internal class ResponseBodyBuilder(
       foreach (var openApiHeader in valueHeaders)
       {
          var nextCellNumber = Cell(1).SetText(openApiHeader.Key)
-            .CellRight(attributesColumnIndex + 1).SetText(Options.Language.Get(openApiHeader.Value.Required))
+            .CellRight(attributesColumnIndex + 1).SetText(Options.Language.Get(openApiHeader.Value.Required)).SetHorizontalAlignment(XLAlignmentHorizontalValues.Center)
             .CellRight().GetColumnNumber();
 
          nextCellNumber = schemaDescriptor.AddSchemaDescriptionValues(openApiHeader.Value.Schema, ActualRow, nextCellNumber);
